9ad5d6d18c6d5af81502ab69e8b25fab72aaff1e
siri-itismytown <siri@itismytown.com>
2025-08-15 08:24:02 +0530

    commitbusprofilenull

--- mytown/Models/businessprofile.cs
+++ mytown/Models/businessprofile.cs
@@ -1,6 +1,7 @@
 ﻿using Stripe;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace mytown.Models
 {
@@ -12,17 +13,17 @@ namespace mytown.Models
         [ForeignKey("BusinessRegister")]
         public int BusRegId { get; set; }
      
-        public string BusinessUsername { get; set; }
-        public string business_location { get; set; }
-        public string business_about { get; set; }
-        public string banner_path { get; set; }
-        public string logo_path { get; set; }
-        public string profile_status { get; set; }
-        public string bus_time { get; set; }
+        public string? BusinessUsername { get; set; }
+        public string? business_location { get; set; }
+        public string? business_about { get; set; }
+        public string? banner_path { get; set; }
+        public string? logo_path { get; set; }
+        public string? profile_status { get; set; }
+        public string? bus_time { get; set; }
         public int BusCatId { get; set; }
         public int BusServId { get; set; }
-        public string Businessservice_name { get; set; }
-        public string Businesscategory_name { get; set; }
+        public string? Businessservice_name { get; set; }
+        public string? Businesscategory_name { get; set; }
 
         public DateTime? approved_date { get; set; }
 
@@ -31,8 +32,8 @@ namespace mytown.Models
         public int image_positiony { get; set; } = 0;
         public float zoom { get; set; } = 1; // Safe default
 
-
-        public virtual BusinessRegister BusinessRegister { get; set; }
+        [JsonIgnore]
+        public virtual BusinessRegister? BusinessRegister { get; set; }
     }
 
 }
