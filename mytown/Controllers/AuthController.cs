146ca657c1555af6fb1de40f2ceb05a955085775
siri-itismytown <siri@itismytown.com>
2026-09-11 19:42:31 +0530

    commt_rolecorrection

--- mytown/Controllers/AuthController.cs
+++ mytown/Controllers/AuthController.cs
@@ -61,7 +61,7 @@ namespace mytown.Controllers
                 return BadRequest(new { error = "Invalid or expired token." });
 
             dynamic record = request;
-            return Ok(new { message = "Valid token", email = record.Email, role = record.role  });
+            return Ok(new { message = "Valid token", email = record.Email, Role = record.role  });
 
         }
 
